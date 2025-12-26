7e3839ae9470fff8938a4376448dba7d4413babf
Ali Yar Khan <aliyark145@gmail.com>
2025-12-26 22:35:29 +0500

    chore: Update build assets, images, and resume, and adjust gitignore.

--- dist/server/assets/_tanstack-start-manifest_v-Be8ei5xh.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/home/aliyar/Documents/freelance/aliyarkhan/src/app/__root.tsx", "children": ["/"], "preloads": ["/assets/main-DgZ44m1q.js"], "assets": [] }, "/": { "filePath": "/home/aliyar/Documents/freelance/aliyarkhan/src/app/index.tsx", "assets": [], "preloads": ["/assets/index-Au5EBhal.js"] } }, "clientEntry": "/assets/main-DgZ44m1q.js" });
-export {
-  tsrStartManifest
-};
